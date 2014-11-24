d05c8a243989bd87fc90fa886ebbc412b0f450a6
Mark Hayter <mark.hayter@gmail.com>
2014-11-23 21:48:41 -0800

    decode in USB input, add Rd

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ var adcButton = document.getElementById("adc");
 var cc1State = document.getElementById("cc1");
 var cc2State = document.getElementById("cc2");
 
-var captureSize = 10000;
-var capBuf = new Uint8Array(captureSize);
+var capSize = 10000;
+var ccEdges = 100;
+var capBuf = new Uint8Array(capSize);
 var capInsert = 0;
 var respCb = null;
 var chOut;
@@ -38,16 +39,29 @@ function keyToTw(event) {
 
 function cmdToTw(cmd, cb) {
   console.log("Send command " + cmd);
-  var a = new ArrayBuffer(cmd.length + 1);
-  var int8View = new Int8Array(a);
-  for (i=0; i<cmd.length; i++) int8View[i] = cmd.charCodeAt(i) & 0xFF;
-  int8View[cmd.length] = 10;
-  var transInfo = { "direction": "out", "endpoint": 1, "data": a };
-  respCb = cb;
-  chrome.usb.bulkTransfer(chOut, transInfo, function() {
-    console.log("Cmd Out done");
-  });
+  var tosend = cmd.length + 1;
+  var sp = 0;
+  while (tosend > 0) {
+    var sblksize = (tosend > 14) ? 14 : tosend;
+    var a = new ArrayBuffer(sblksize);
+    var cbuf = new Int8Array(a);
+    for (i=0; i<(sblksize-1); i++) cbuf[i] = cmd.charCodeAt(sp++) & 0xFF;
+    cbuf[sblksize-1] = (sp == cmd.length) ? 10 : cmd.charCodeAt(sp++) & 0xFF;
+    var transInfo = { "direction": "out", "endpoint": 1, "data": a };
+    respCb = cb;
+    chrome.usb.bulkTransfer(chOut, transInfo, function() {
+      console.log("Cmd Out done");
+    });
+    tosend -= sblksize;
+  }
 }
+
+var capCC = 0;
+var capLast = new Array(0,0,0,0);
+var capPad = new Array(0,0,0,0);
+// buffer zone of 64 edges in worst case
+var capDelta = new Array(new Uint32Array(ccEdges+64), new Uint32Array(ccEdges+64));
+var capPos = new Array(0,0,0,0);
 
 function onCapIn(info) {
   if (info.resultCode != 0) {
@@ -55,32 +69,81 @@ function onCapIn(info) {
       console.log(chrome.runtime.lastError);
       if (info.resultCode != 6) return;
   } else {
-    console.log("onCapIn: length " + info.data.byteLength);
+    //console.log("onCapIn: length " + info.data.byteLength);
     var intView = new Uint8Array(info.data)
     for(i=0; i < info.data.byteLength; i++) {
-      capBuf[capInsert++] = intView[i];
+      var dat = intView[i];
+      if ((capInsert & 63) == 1) capCC = (dat>>4) & 3;
+      if ((capInsert & 63) > 3) {
+        if (dat == capLast[capCC]) {
+          capPad[capCC]++;
+        } else {
+          var pad = 256 * capPad[capCC];
+          if ((dat < capLast[capCC]) || (dat == 0xff)) pad += 256;
+          if (capCC < 2)
+            capDelta[capCC][capPos[capCC]++] = pad + dat - capLast[capCC];
+          capLast[capCC] = dat;
+          capPad[capCC] = 0;
+        }
+      }
+      if (capInsert < capSize) capBuf[capInsert] = dat;
+      capInsert++;
     }
   }
-  if (capInsert < (captureSize - 64)) {
+  //if (capInsert < (capSize - 64)) {
+  if ((capPos[0] < ccEdges) && (capPos[1] < ccEdges)) {
     chrome.usb.bulkTransfer(chCap, capInTransInfo, onCapIn);
+    return;
   } else {
     var dump = "";
     var lastTime = 0;
-    for (i=0; i < capInsert; i++) {
+    var cc = 0;
+    var ccstate = new Array(0,0,0,0);
+    var cctime = new Array(0,0,0,0);
+    var cclastval = new Array(0,0,0,0);
+    var ccpad = new Array(0,0,0,0);
+    var ccdump = new Array("", "", "", "");
+    var cmp = new Array(0,0,0,0);
+    var dumpsize = (capInsert > capSize) ? capSize : capInsert;
+
+    for (i=0; i < dumpsize; i++) {
       if ((i & 63) == 0) {
         var sampleTime = (capBuf[i+3]<<8) + capBuf[i+2];
         var timeDiff = (i==0) ? 0 : sampleTime - lastTime;
+        var seqf = (capBuf[i+1]<<8) + capBuf[i];
+        cc = (seqf >> 12) & 0x3;
+        var seq = seqf & 0xfff;
+        var ofw = (seqf & 0x8000) ? "*" : " ";
         lastTime = sampleTime;
-        dump += "<br/>" + sampleTime + " " + capBuf[i+1].toString(16) + " " + timeDiff;
+        // dump += "<br/>CC" + (cc+1) + " " + sampleTime + " " + capBuf[i+1].toString(16) + " " + timeDiff;
+        dump += "<br/>CC" + (cc+1) + " " + ofw + seq + " " + seqf.toString(16);
         //i += 3;
       } //else {
+      if ((i & 63) > 3) {
+        if (capBuf[i] == cclastval[cc]) {
+          ccpad[cc]++;
+        } else {
+          var pad = 256 * ccpad[cc];
+          if ((capBuf[i] < cclastval[cc]) || (capBuf[i] == 0xff)) pad += 256;
+          var deltat = pad + capBuf[i] - cclastval[cc];
+          cctime[cc] += deltat;
+          cclastval[cc] = capBuf[i];
+          ccpad[cc] = 0;
+          ccstate[cc] = ccstate[cc] ^ 1;
+          ccdump[cc] += ccstate[cc] + " +" + deltat + " " + capDelta[cc][cmp[cc]++] + " " + cctime[cc] + "<br>";
+          dump += ccstate[cc] + " +" + deltat + " ";
+        }
+      }
         if (capBuf[i] < 16)
           dump += " 0" + capBuf[i].toString(16);
         else
           dump += " " + capBuf[i].toString(16);
       //}
     }
-    document.getElementById("capturedump").innerHTML = dump;
+    for(i=0; i < 2; i++) while (cmp[i] < capPos[i]) {
+        ccdump[i] += ". +" + capDelta[i][cmp[i]++] + "<BR>";
+    }
+    document.getElementById("capturedump").innerHTML = dump + "<BR><B>CC1</B><BR>" + ccdump[0] + "<BR><B>CC2</B><BR>" + ccdump[1];
     chrome.usb.releaseInterface(chCap, 3, function() {
     console.log("Released interface 3");
   });
@@ -194,6 +257,7 @@ var permissionObj = {permissions: [{'usbDevices': [DEVICE_INFO] }]};
 
 captureButton.addEventListener('click', function() {
   capInsert = 0;
+  capPos[0] = capPos[1] = capPos[2] = capPos[3] = 0;
   chrome.usb.claimInterface(chCap, 3, function() {
     console.log("Claimed interface 3");
     chrome.usb.bulkTransfer(chCap, capInTransInfo, onCapIn);
@@ -236,6 +300,16 @@ adcButton.addEventListener('click', function() {
         cc2State.innerHTML = ccAdcToRval(res1);
     });
   });
+});
+
+var rd1Button = document.getElementById("rd1");
+
+rd1Button.addEventListener('click', function() {
+  console.log("Got rd1 state is " + rd1Button.checked);
+  if (rd1Button.checked)
+    cmdToTw("gpioset CC1_RD 0", function(){});
+  else
+    cmdToTw("gpioset CC1_RD 1", function(){});
 });
 
 
